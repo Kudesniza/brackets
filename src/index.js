37d4ae9239ae6b744b808819eef1a722dd6c2558
Kudesniza <kudesniza@yandex.ru>
2020-10-19 21:56:02 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+let test = 0
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const holder = []
+  let sameObject = ['7', '8', '|']
+  const openBrackets = ['(','{','[', '5', '1', '3']
+  const closedBrackets = [')','}',']', '6', '2', '4']
+  
+  for (let i=0; i<str.length; i++) { // loop trought all letters of expr
+    if (sameObject.includes(str[i])) {
+      if (holder[holder.length - 1] === str[i]) {
+        holder.splice(-1,1)
+      } else {
+        holder.push(str[i])
+      }
+      } else {
+        if(openBrackets.includes(str[i])){ // if its oppening bracket
+        holder.push(str[i])
+        }else if(closedBrackets.includes(str[i])){ // if its closing
+          const openPair = openBrackets[closedBrackets.indexOf(str[i])] // find his pair
+        if(holder[holder.length - 1] === openPair){ // check if that pair is last element in array
+        holder.splice(-1,1) //if so, remove it
+        }else{ // if its not
+        holder.push(str[i])
+        break // exit loop
+      }
+    }
+  }
+}
+return (holder.length === 0)
 }
